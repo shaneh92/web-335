4d7e9fdab808e2fff756ca675318853534521125
Shane Hingtgen <shanehingtgen@gmail.com>
2023-03-20 21:41:20 -0500

    backing up

---
+++ week-2/app.py
@@ -0,0 +1,108 @@
+# variables
+# age = 20
+# # first_name = 'John'
+# # last_name = 'Smith'
+# # new_patient = True
+# # print(age)
+
+#recieving input
+# name = input("What is your name?")
+# print("Hello " + name)
+
+# typer conversion
+# birth_year = input("Enter your birth year:")
+# age = 2023 - int(birth_year)
+# print(age)
+#
+# int()
+# float()
+# bool()
+# str()
+
+# print("Calculate the sum of two numbers")
+# first_number = float(input("First Number:"))
+# second_number = float(input("Second Number"))
+#
+# sum = first_number + second_number
+# print("Sum is: " + str(sum))
+
+# strings
+# course = "Python for Beginners"
+# print('Python' in course)
+
+# arithmetic operators
+# x = 10
+# x += 3
+
+# operator precedence
+# x = (10 + 3) * 2
+# print(x)
+
+# comparison operators
+# x = 3 > 2
+# print(x)
+# > >= < <= == !=
+
+# logical operators
+# price = 5
+# print(not price > 10)
+#  and
+# or
+#  not
+
+# if statements
+# temperature = 1
+# if temperature > 30:
+#     print("It's a hot day")
+#     print("Drink water")
+# elif temperature > 20:
+#     print("It's a nice day")
+# elif temperature > 10:
+#     print("It's a bit cold")
+# else:
+#     print("it's cold")
+# print('Done')
+
+
+# weight = float(input("Weight: "))
+# unit = input("(K)g or (L)bs? ")
+#
+# if unit.upper() == "K":
+#     converted = weight / 0.45
+#     print("Weight in Lbs: " + str(converted))
+# else:
+#     converted = weight * 0.45
+#     print("Weight in Kgs: " + str(converted))
+
+# while loops
+# i = 1
+# while i <= 10:
+#     print(i * '*')
+#     i += 1
+
+# lists
+# names = ["John", "Bob", "Mosh", "Sam", "Marry"]
+# names[0] = "Jon"
+# print(names[0:3])
+# print(names)
+
+# list methods
+# numbers = [1, 2, 3, 4, 5]
+# print(len(numbers))
+
+# for loops
+# numbers = [1, 2, 3, 4, 5]
+#
+# for item in numbers:
+#     print(item)
+
+# numbers = range(5, 10, 2)
+#
+# for number in numbers:
+#     print(number)
+# or
+# for number in range(5):
+#     print(number)
+
+# tuples
+# numbers = (1, 2, 3)
---
+++ week-2/hingtgen_myworld.py
@@ -0,0 +1,13 @@
+# =================================================================================================================
+# ; Title: Exercise 2.3 - Python
+# ; Author: Shane Hingtgen
+# ; Bellevue University
+# ; Date: 03/20/2023
+# ; Description: In this exercise we install python and watch a video about python basics and print to the console
+# ; Work Cited:
+#   Web 335 Python Guide
+#   Web 335 Install Python Course Video
+#   Python for Beginners https://www.youtube.com/watch?v=kqtD5dpn9C8&t=3594s
+#   Web 335 Exercise 2
+# =================================================================================================================
+print("You are now in Hingtgen's world!")
